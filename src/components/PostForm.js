afb53622ad329f186a33733659d585c8188051f1
Elias <elias.chehaibou@gmail.com>
2021-04-27 16:35:49 +0200

    v1.1

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -18,7 +18,7 @@ const PostForm = () => {
     <div className="for-container">
     <h1>{posts[0].nom}</h1>
       <form onSubmit={(e) => handleForm(e)}>
-        <Question posts= {posts} valeur = {0} name = {"1"} />
+        <Question posts= {posts} valeur = {0} name = {"1"}/>
         <Question posts= {posts} valeur = {1} name = {"2"}/>
         <Question posts= {posts} valeur = {2} name = {"3"}/>
         <input type="submit" value="Envoyer" />
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 const Question = ({posts, valeur, name}) => {
+    if (posts[0].type=="oui/non") {
+        return (
+            <div>
+                <p>{posts[0].questions[valeur].text}</p>
+                <input
+                type="radio"
+                name={name}
+                id="1"
+                />
+                <label>oui</label>
+                <input
+                type="radio"
+                name={name}
+                id="2"
+                />
+                <label>non</label>           
+            </div>
+        )}
     return (
         <div>
             <p>{posts[0].questions[valeur].text}</p>
@@ -9,15 +27,27 @@ const Question = ({posts, valeur, name}) => {
             name={name}
             id="1"
             />
-            <label>oui</label>
+            <label>{posts[0].questions[valeur].answers[0].answerText}</label>
             <input
             type="radio"
             name={name}
             id="2"
             />
-            <label>non</label>           
+            <label>{posts[0].questions[valeur].answers[1].answerText}</label>
+            <input
+            type="radio"
+            name={name}
+            id="3"
+            />
+            <label>{posts[0].questions[valeur].answers[1].answerText}</label>
+            <input
+            type="radio"
+            name={name}
+            id="4"
+            />
+            <label>{posts[0].questions[valeur].answers[0].answerText}</label>           
         </div>
-    );
+    )
 };
 
 export default Question;
